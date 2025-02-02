d6952d94fe8438b316842a13cc06260de4284865
Meni Yakove <441263+myakove@users.noreply.github.com>
2025-02-02 16:14:08 +0200

    add support for stop the sampler on specific exceptions (#122)
    * feat(TimeoutSampler): add support for stop the sampler on specific exceptions.
    
    - Added new test cases for `TimeoutSampler` to validate the functionality of raising exceptions based on user-defined conditions.
    - Updated the `TimeoutSampler` class to include a new parameter `raise_on_exceptions`, which allows specifying exceptions that should be raised immediately when encountered.
    - Modified the `_should_raise_on_exception` method to handle the new logic for raising exceptions based on user-defined messages.
    - Added helper methods `_should_ignore_exception` and `_elapsed_time_log` to improve readability and maintainability of the code.
    
    * Fix the tests
    
    * Fix typo in test names
    
    * Update the README
    
    * Fix logic in examples [README]

--- tests/test_timeout_sampler.py
+++ tests/test_timeout_sampler.py
@@ -69,7 +69,7 @@ class TestTimeoutSampler:
             pytest.param(
                 {},
                 {
-                    "exception_log_regex": "^.*\nLast exception: N/A: None$",
+                    "exception_log_regex": "^.*\nLast exception: N/A",
                 },
                 id="noargs_timeout_only",
             ),
@@ -78,7 +78,7 @@ class TestTimeoutSampler:
                     "runtime_exception": Exception(),
                 },
                 {
-                    "exception_log_regex": "^.*\nLast exception: Exception: $",
+                    "exception_log_regex": "^.*\nLast exception: Exception:",
                 },
                 id="noargs_raise_exception_with_no_msg",
             ),
@@ -87,7 +87,7 @@ class TestTimeoutSampler:
                     "runtime_exception": ValueError(),
                 },
                 {
-                    "exception_log_regex": "^.*\nLast exception: ValueError: $",
+                    "exception_log_regex": "^.*\nLast exception: ValueError:",
                 },
                 id="noargs_raise_valueerror_with_no_msg",
             ),
@@ -99,7 +99,7 @@ class TestTimeoutSampler:
                     "runtime_exception": ValueError("test"),
                 },
                 {
-                    "exception_log_regex": "^.*\nLast exception: ValueError: test$",
+                    "exception_log_regex": "^.*\nLast exception: ValueError: test",
                 },
                 id="init_valueerror_with_msg_raise_valueerror_with_allowed_msg",
             ),
@@ -113,7 +113,7 @@ class TestTimeoutSampler:
                     "runtime_exception": IndexError("my allowed exception text"),
                 },
                 {
-                    "exception_log_regex": ("^.*\nLast exception: IndexError: my allowed exception text$"),
+                    "exception_log_regex": ("^.*\nLast exception: IndexError: my allowed exception text"),
                 },
                 id="init_multi_exceptions_raise_allowed_with_allowed_msg",
             ),
@@ -137,7 +137,7 @@ class TestTimeoutSampler:
 
 
 def test_sampler():
-    sampler = TimeoutSampler(wait_timeout=1, sleep=1, func=lambda: True)
+    sampler = TimeoutSampler(wait_timeout=1, sleep=1, print_log=False, func=lambda: True)
     for sample in sampler:
         if sample:
             return
@@ -150,6 +150,7 @@ def test_sampler_negative():
         wait_timeout=10,
         sleep=1,
         func=lambda: False,
+        print_log=False,
     )
     with pytest.raises(TimeoutExpiredError):
         for sample in sampler:
@@ -160,12 +161,20 @@ def test_sampler_negative():
 # retry decorator tests
 
 
-@retry(wait_timeout=1, sleep=1)
+@retry(
+    wait_timeout=1,
+    sleep=1,
+    print_log=False,
+)
 def always_succeeds():
     return True
 
 
-@retry(wait_timeout=1, sleep=1)
+@retry(
+    wait_timeout=1,
+    sleep=1,
+    print_log=False,
+)
 def never_succeeds():
     return False
 
@@ -177,3 +186,64 @@ def test_decorator():
 def test_decorator_negative():
     with pytest.raises(TimeoutExpiredError):
         never_succeeds()
+
+
+# Test raise_on_exceptions
+def raise_on_exception():
+    raise ValueError()
+
+
+def raise_on_exception_with_msg():
+    raise ValueError("ValueError")
+
+
+def test_raise_on_exception():
+    with pytest.raises(TimeoutExpiredError):
+        for sample in TimeoutSampler(
+            func=raise_on_exception,
+            wait_timeout=2,
+            sleep=1,
+            raise_on_exceptions={ValueError: []},
+            print_log=False,
+        ):
+            if sample:
+                return
+
+
+def test_raise_on_exception_with_msg():
+    with pytest.raises(TimeoutExpiredError):
+        for sample in TimeoutSampler(
+            func=raise_on_exception_with_msg,
+            wait_timeout=2,
+            sleep=1,
+            raise_on_exceptions={ValueError: ["ValueError"]},
+            print_log=False,
+        ):
+            if sample:
+                return
+
+
+def test_raise_on_exception_with_wrong_msg():
+    with pytest.raises(TimeoutExpiredError, match=re.compile(r"Timed Out: 2", re.DOTALL)):
+        for sample in TimeoutSampler(
+            func=raise_on_exception_with_msg,
+            wait_timeout=2,
+            sleep=1,
+            raise_on_exceptions={ValueError: ["Wrong"]},
+            print_log=False,
+        ):
+            if sample:
+                return
+
+
+def test_raise_on_exception_with_wrong_exception():
+    with pytest.raises(TimeoutExpiredError, match=re.compile(r"Timed Out: 2|Last exception: ValueError: ValueError")):
+        for sample in TimeoutSampler(
+            func=raise_on_exception_with_msg,
+            wait_timeout=2,
+            sleep=1,
+            raise_on_exceptions={TypeError: []},
+            print_log=False,
+        ):
+            if sample:
+                return
--- timeout_sampler/__init__.py
+++ timeout_sampler/__init__.py
@@ -9,14 +9,24 @@ from simple_logger.logger import get_logger
 LOGGER = get_logger(name=__name__)
 
 
+def _elapsed_time_log(elapsed_time: float) -> str:
+    return f"Elapsed time: {elapsed_time} [{datetime.timedelta(seconds=elapsed_time)}]"
+
+
 class TimeoutExpiredError(Exception):
-    def __init__(self, value: str, last_exp: Exception | None = None) -> None:
+    def __init__(self, value: str, last_exp: Exception | None = None, elapsed_time: float | None = None) -> None:
         super().__init__()
         self.value = value
         self.last_exp = last_exp
+        self.elapsed_time = elapsed_time
 
     def __str__(self) -> str:
-        return f"Timed Out: {self.value}"
+        msg = f"Timed Out: {self.value}."
+
+        if self.elapsed_time:
+            msg += f"\n{_elapsed_time_log(elapsed_time=self.elapsed_time)}"
+
+        return msg
 
 
 class TimeoutSampler:
@@ -29,45 +39,49 @@ class TimeoutSampler:
 
     Yielding the output allows you to handle every value as you wish.
 
-    exceptions_dict should be in the following format:
-    {
-        exception0: [exception0_msg0],
-        exception1: [
-            exception1_msg0,
-            exception1_msg1
-        ],
-        exception2: []
-    }
+    exceptions_dict and raise_on_exceptions:
+        exceptions_dict should be in the following format:
+        {
+            exception0: [exception0_msg0],
+            exception1: [
+                exception1_msg0,
+                exception1_msg1
+            ],
+            exception2: []
+        }
 
-    If an exception is raised within `func`:
-        Example exception inheritance:
-            class Exception
-            class AExampleError(Exception)
-            class BExampleError(AExampleError)
+    exceptions_dict:
+        If an exception is raised within `func`:
+            Example exception inheritance:
+                class Exception
+                class AExampleError(Exception)
+                class BExampleError(AExampleError)
 
-        The raised exception's class will fall into one of three categories:
-            1. An exception class specifically declared in exceptions_dict
-                exceptions_dict: {BExampleError: []}
-                raise: BExampleError
-                result: continue
+            The raised exception's class will fall into one of three categories:
+                1. An exception class specifically declared in exceptions_dict
+                    exceptions_dict: {BExampleError: []}
+                    raise: BExampleError
+                    result: continue
 
-            2. A child class inherited from an exception class in exceptions_dict
-                exceptions_dict: {AExampleError: []}
-                raise: BExampleError
-                result: continue
+                2. A child class inherited from an exception class in exceptions_dict
+                    exceptions_dict: {AExampleError: []}
+                    raise: BExampleError
+                    result: continue
 
-            3. Everything else, this will always re-raise the exception
-                exceptions_dict: {BExampleError: []}
-                raise: AExampleError
-                result: raise
+                3. Everything else, this will always re-raise the exception
+                    exceptions_dict: {BExampleError: []}
+                    raise: AExampleError
+                    result: raise
+
 
     Args:
         wait_timeout (int): Time in seconds to wait for func to return a value equating to True
         sleep (int): Time in seconds between calls to func
         func (Callable): to be wrapped by TimeoutSampler
-        exceptions_dict (dict): Exception handling definition
+        exceptions_dict (dict): Exception handling definition, only exception that specifically raised in exceptions_dict will be ignored
         print_log (bool): Print elapsed time to log.
         print_func_log (bool): Add function call info to log
+        raise_on_exceptions (dict): Exceptions that should be raised immediately.
     """
 
     def __init__(
@@ -79,6 +93,7 @@ class TimeoutSampler:
         print_log: bool = True,
         print_func_log: bool = True,
         func_args: tuple[Any] | None = None,
+        raise_on_exceptions: dict[type[Exception], list[str]] | None = None,
         **func_kwargs: Any,
     ):
         self.wait_timeout = wait_timeout
@@ -88,8 +103,8 @@ class TimeoutSampler:
         self.func_kwargs = func_kwargs or {}
         self.print_log = print_log
         self.print_func_log = print_func_log
-        self.exceptions_dict: dict[type[Exception], list[str]] = exceptions_dict or {Exception: []}
-        self._exceptions = tuple(self.exceptions_dict.keys())
+        self.exceptions_dict = exceptions_dict or {Exception: []}
+        self.raise_on_exceptions = raise_on_exceptions or {}
 
     def _get_func_info(self, _func: Callable, type_: str) -> Any:
         # If func is partial function.
@@ -152,15 +167,23 @@ class TimeoutSampler:
             except Exception as exp:
                 last_exp = exp
                 elapsed_time = self.wait_timeout - timeout_watch.remaining_time()
-                if self._should_raise_by_exception(exp=last_exp):
-                    raise TimeoutExpiredError(self._get_exception_log(exp=last_exp), last_exp=last_exp)
+
+                if self._should_raise_on_exception(exp=last_exp):
+                    raise TimeoutExpiredError(
+                        self._get_exception_log(exp=last_exp), last_exp=last_exp, elapsed_time=elapsed_time
+                    )
+
+                if not self._should_ignore_exception(exp=last_exp):
+                    raise TimeoutExpiredError(
+                        self._get_exception_log(exp=last_exp), last_exp=last_exp, elapsed_time=elapsed_time
+                    )
 
                 time.sleep(self.sleep)
                 elapsed_time = None
 
             finally:
                 if self.print_log and elapsed_time:
-                    LOGGER.info(f"Elapsed time: {elapsed_time} [{datetime.timedelta(seconds=elapsed_time)}]")
+                    LOGGER.info(_elapsed_time_log(elapsed_time=elapsed_time))
 
         raise TimeoutExpiredError(self._get_exception_log(exp=last_exp), last_exp=last_exp)
 
@@ -183,7 +206,24 @@ class TimeoutSampler:
         # Prevent match if provided with empty string
         return any(msg and msg in str(exp) for msg in exception_messages)
 
-    def _should_raise_by_exception(self, exp: Exception) -> bool:
+    def _should_raise_on_exception(self, exp: Exception) -> bool:
+        """
+        Check if exception should be raised during execution of `func` by exceptions that
+        sent by the user (self.raise_on_exceptions)
+        """
+        for entry in self.raise_on_exceptions:
+            if isinstance(exp, entry):  # Check inheritance for raised exception
+                exception_messages = self.raise_on_exceptions.get(entry, [])
+
+                if not exception_messages:
+                    return True
+
+                if self._is_exception_matched(exp=exp, exception_messages=exception_messages):
+                    return True
+
+        return False
+
+    def _should_ignore_exception(self, exp: Exception) -> bool:
         """
         Verify whether exception should be raised during execution of `func`
 
@@ -198,9 +238,9 @@ class TimeoutSampler:
             if isinstance(exp, entry):  # Check inheritance for raised exception
                 exception_messages = self.exceptions_dict.get(entry, [])
                 if self._is_exception_matched(exp=exp, exception_messages=exception_messages):
-                    return False
+                    return True
 
-        return True
+        return False
 
     def _get_exception_log(self, exp: Exception | None = None) -> str:
         """
@@ -213,8 +253,11 @@ class TimeoutSampler:
             str: Log message for exception
         """
         exp_name = exp.__class__.__name__ if exp else "N/A"
+        last_exception_log = f"Last exception: {exp_name}"
 
-        last_exception_log = f"Last exception: {exp_name}: {exp}"
+        if exp:
+            last_exception_log += f": {exp}"
+
         return f"{self.wait_timeout}\n{self._func_log if self.print_func_log else ''}\n{last_exception_log}"
 
 
@@ -242,6 +285,7 @@ def retry(
     exceptions_dict: dict[type[Exception], list[str]] | None = None,
     print_log: bool = True,
     print_func_log: bool = True,
+    raise_on_exceptions: dict[type[Exception], list[str]] | None = None,
 ) -> Callable:
     """
     Decorator for TimeoutSampler, For usage see TimeoutSampler.
@@ -264,6 +308,7 @@ def retry(
                 print_log=print_log,
                 print_func_log=print_func_log,
                 func_args=args,
+                raise_on_exceptions=raise_on_exceptions,
                 **kwargs,
             ):
                 if sample:
